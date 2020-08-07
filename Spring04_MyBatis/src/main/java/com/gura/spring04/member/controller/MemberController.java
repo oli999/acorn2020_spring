84da92e3d87621d12a8b78affac4e8830791cfca
kimgura <oli999@naver.com>
2020-08-07 11:57:13 +0900

    Spring04_MyBatis 에 회원 목록 보기 기능 구현함

--- Spring04_MyBatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
+++ Spring04_MyBatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
@@ -1,23 +1,31 @@
 package com.gura.spring04.member.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring04.member.dao.MemberDao;
 import com.gura.spring04.member.dto.MemberDto;
-
 
 @Controller
 public class MemberController {
+	//의존객체를 주입 받는다.
+	@Autowired
+	private MemberDao dao;
 	
-	
-	
-	//회원 정보 추가 요청 처리 
-	@RequestMapping("/member/insert")
-	public ModelAndView insert(MemberDto dto) {
-		
-		return null;
+	@RequestMapping("/member/list")
+	public ModelAndView list(ModelAndView mView) {
+		//회원목록을 얻어온다.
+		List<MemberDto> list=dao.getList();
+		//model 을 담는다
+		mView.addObject("list", list);
+		//view 페이지 정보를 담고 
+		mView.setViewName("member/list");
+		//ModelAndView 객체를 리턴해 준다.
+		return mView;
 	}
 }
 
---
+++ Spring04_MyBatis/src/main/webapp/WEB-INF/views/member/list.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>/views/member/list.jsp</title>
+<link rel="stylesheet" href="${pageContext.request.contextPath }/resources/css/bootstrap.css" />
+</head>
+<body>
+<div class="container">
+	<h1>회원 목록 입니다.</h1>
+	<table class="table">
+		<thead class="thead-dark">
+			<tr>
+				<th>번호</th>
+				<th>이름</th>
+				<th>주소</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach var="tmp" items="${list }">
+				<tr>
+					<td>${tmp.num }</td>
+					<td>${tmp.name }</td>
+					<td>${tmp.addr }</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
+
+
+
