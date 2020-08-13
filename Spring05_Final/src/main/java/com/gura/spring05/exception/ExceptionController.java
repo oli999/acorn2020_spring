2b4e37677d3647f95fba4cac5350cb17eed06362
kimgura <oli999@naver.com>
2020-08-13 19:02:08 +0900

    Spring05 예외 처리 관련 필기 추가함

--- Spring05_Final/src/main/java/com/gura/spring05/exception/ExceptionController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/exception/ExceptionController.java
@@ -5,17 +5,28 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+//예외 컨트롤러를 bean 으로 만들기 위한 어노테이션 
 @ControllerAdvice
 public class ExceptionController {
 	
+	//spring framework 가 동작하는 중에 NotDeleteException type 의 
+	//예외가 발생하면 호출되는 메소드 
 	@ExceptionHandler(NotDeleteException.class)
 	public ModelAndView notDelete(NotDeleteException nde) {
+		//해당 오류가 발생했을때 원하는 작업을 한후 
+		
+		//view page 로 forward 이동해서 예외 정보를 응답한다. 
 		ModelAndView mView=new ModelAndView();
+		//exception  이라는 키값으로 예외 객체를 담고 
 		mView.addObject("exception", nde);
+		// /WEB-INF/views/error/info.jsp 페이지로 forward 이동 
 		mView.setViewName("error/info");
 		return mView;
 	}
 }
+
+
+
 
 
 
--- Spring05_Final/src/main/java/com/gura/spring05/exception/NotDeleteException.java
+++ Spring05_Final/src/main/java/com/gura/spring05/exception/NotDeleteException.java
@@ -1,5 +1,9 @@
 package com.gura.spring05.exception;
-
+/*
+ *  일반적인 예외 클래스는 RuntimeException 클래스를 상속 받아서 만든다.
+ *  - 생성자의 인자로 예외 메세지를 전달 받아서 부모 생성자에 넘겨주면
+ *    .getMessage() 메소드를 호출할때 예외 넘겨주었던 예외 메세지가 리턴된다. 
+ */
 public class NotDeleteException extends RuntimeException{
 	//생성자
 	public NotDeleteException(String message) {
@@ -7,3 +11,15 @@ public class NotDeleteException extends RuntimeException{
 		super(message);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
