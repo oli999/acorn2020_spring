e9d75038efb09d1a48c1159bba218e03dc238ae4
kimgura <oli999@naver.com>
2020-08-14 12:23:13 +0900

    Spring05 카페글 목록보기 구현중...

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
@@ -1,7 +1,11 @@
 package com.gura.spring05.cafe.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.cafe.service.CafeService;
 
@@ -9,6 +13,14 @@ import com.gura.spring05.cafe.service.CafeService;
 public class CafeController {
 	@Autowired
 	private CafeService cafeService;
+	//카페 글 목록 보기 요청 처리 
+	@RequestMapping("/cafe/list")
+	public ModelAndView getList(HttpServletRequest request, 
+			ModelAndView mView) {
+		
+		mView.setViewName("cafe/list");
+		return mView;
+	}
 	
 }
 
--- Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeServiceImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gura.spring05.cafe.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,11 @@ import com.gura.spring05.cafe.dao.CafeDao;
 public class CafeServiceImpl implements CafeService{
 	@Autowired
 	private CafeDao cafeDao;
+
+	@Override
+	public void getList(HttpServletRequest request) {
+		
+	}
 	
 }
 
