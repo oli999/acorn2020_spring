ff9a00f54a603b9f8761bc32ee1e12dac0240bb0
kimgura <oli999@naver.com>
2020-08-24 14:58:58 +0900

    Spring05 log4j 오류수정, TimeAspect 예제 추가함

--- Spring05_Final/src/main/java/com/gura/spring05/aspect/TimeAspect.java
+++ Spring05_Final/src/main/java/com/gura/spring05/aspect/TimeAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 
 @Aspect
 @Component
@@ -16,19 +17,21 @@ public class TimeAspect {
 	@Around("execution(* com.gura.spring05.file.controller.*.*(..))")
 	public Object checkTime(ProceedingJoinPoint joinPoint) throws Throwable {
 		//로그를 출력할 객체의 참조값 얻어오기 
-		Logger logger=LoggerFactory.getLogger(joinPoint.getClass());
-		//logger.info("요청처리 시작!");
-		System.out.println("요청 처리 시작!");
+		//jointPoint.getTarget().getClass() => aop 가 적용된 클래스의 type
+		Logger logger=
+				LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+		logger.info("요청 처리 시작!");
+		//시작시간 (ms)
 		long startTime=System.currentTimeMillis();
-		
+
 		Object obj=joinPoint.proceed(); //aop 가 적용된 메소드 수행하기 
-		
+		//끝시간 (ms)
 		long endTime=System.currentTimeMillis();
+		//시작과 끝의 차이를 얻어낸다. 
 		long time=endTime-startTime;
+		logger.info("time:"+time);
+		logger.info("요청 처리 끝!");
 		
-		System.out.println("요청 처리에 소요된 시간:"+time);
-		System.out.println("요청 처리 끝!");
-		//logger.info("요청처리 끝!");
 		return obj;
 	}
 }
